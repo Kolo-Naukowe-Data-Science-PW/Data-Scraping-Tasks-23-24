2d6ed949951096e90c4bfb52b0f7f70253917cea
matey42 <4matey2@gmail.com>
2023-12-06 14:54:27 +0100

    refactor: fix style issues reported in pull request

--- otodom/task_1/mk/otodom.py
+++ otodom/task_1/mk/otodom.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import re
 import sys
@@ -15,151 +16,187 @@ HEADERS = {
 
 
 class Crawler:
-    def getPage(self, url):
+    def getPage(self, url: str, params: dict = {}):
         try:
-            req = requests.get(url, headers=HEADERS)
-        except requests.exceptions.RequestException:
-            print(f"Failed to retrieve the webpage. Status code: {req.status_code}")
-            return None
-        return BeautifulSoup(req.text, "html.parser")
+            req = requests.get(url, params=params, headers=HEADERS)
+            req.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to retrieve the webpage. Error: {e}", file=sys.stderr)
+            return (None, None)
 
-    def getLinks(self, url):
-        bs = self.getPage(url)
+        return (BeautifulSoup(req.text, "html.parser"), req.url)
+
+    def getLinks(self, url: str):
+        bs = self.getPage(url)[0]
+
         if bs is not None:
+            promoted_urls = []
             promoted_listings = bs.find(
                 "div", {"data-cy": "search.listing.promoted"}
-            ).find_all("a", {"class": "css-1hfdwlm e1dfeild2"})
-            promoted = []
+            ).find_all("a", {"data-cy": "listing-item-link"})
             for link in promoted_listings:
                 if "href" in link.attrs:
-                    promoted.append(link.attrs["href"])
+                    promoted_urls.append(link.attrs["href"])
 
+            organic_urls = []
             organic_listings = bs.find(
                 "div", {"data-cy": "search.listing.organic"}
-            ).find_all("a", {"class": "css-1hfdwlm e1dfeild2"})
-            organic = []
+            ).find_all("a", {"data-cy": "listing-item-link"})
             for link in organic_listings:
                 if "href" in link.attrs:
-                    organic.append(link.attrs["href"])
+                    organic_urls.append(link.attrs["href"])
 
-            return (promoted, organic)
+            return (promoted_urls, organic_urls)
         return None
 
-    def getListing(self, pageUrl, promoted):
+    def getListing(self, pageUrl: str, promoted: bool):
         url = "http://www.otodom.pl{}".format(pageUrl)
-        bs = self.getPage(url)
+        bs = self.getPage(url)[0]
+
         if bs is not None:
             listing = dict()
             listing["url"] = url
 
-            id_match = re.search(
-                "[0-9]+$", bs.find("meta", {"name": "description"}).get("content", "")
-            )
-            listing["otodom_id"] = id_match.group() if id_match else ""
+            try:
+                id_match = re.search(
+                    "[0-9]+$",
+                    bs.find("meta", {"name": "description"}).get("content", ""),
+                )
+                listing["otodom_id"] = id_match.group() if id_match else ""
 
-            listing["title"] = bs.find("h1", {"class": "css-1wnihf5 efcnut38"}).text
+                listing["title"] = bs.find("h1", {"data-cy": "adPageAdTitle"}).text
 
-            localization = dict()
-            l10n = bs.find("a", {"class": "e1w8sadu0 css-1helwne exgq9l20"}).text.split(
-                ","
-            )
+                localization = dict()
+                adres = bs.find("a", {"aria-label": "Adres"}).text.split(",")
 
-            localization["province"] = l10n[-1] if len(l10n) >= 4 else ""
-            localization["city"] = l10n[-2] if len(l10n) >= 4 else l10n[-1]
-            localization["district"] = l10n[1] if len(l10n) >= 4 else ""
-            localization["street"] = l10n[0] if len(l10n) >= 4 else l10n[0]
+                match len(adres):
+                    case x if x >= 4:
+                        localization["province"] = adres[-1].strip()
+                        localization["city"] = adres[-2].strip()
+                        localization["district"] = adres[1].strip()
+                        localization["street"] = adres[0].strip()
+                    case _:
+                        localization["province"] = ""
+                        localization["city"] = adres[-1].strip()
+                        localization["district"] = ""
+                        localization["street"] = adres[0].strip()
 
-            listing["localization"] = localization
+                listing["localization"] = localization
 
-            listing["promoted"] = promoted
-            number = bs.find("strong", {"class": "css-t3wmkv e1l1avn10"}).text.replace(
-                ",", "."
-            )
-            listing["price"] = int(float(re.sub(r"[^.0-9]", "", number)))
-            number = bs.find(
-                "div", {"data-testid": "table-value-rooms_num"}
-            ).text.replace(",", ".")
-            listing["rooms"] = int(float(re.sub(r"[^.0-9]", "", number)))
-            number = bs.find("div", {"data-testid": "table-value-area"}).text.replace(
-                ",", "."
-            )
-            listing["area"] = int(float(re.sub(r"[^.0-9]", "", number)))
-            listing["estate_agency"] = bs.find(
-                "div", {"data-testid": "table-value-advertiser_type"}
-            ).text
+                listing["promoted_urls"] = promoted
 
-            return listing
+                number = bs.find("strong", {"aria-label": "Cena"}).text.replace(
+                    ",", "."
+                )
+                listing["price"] = int(float(re.sub(r"[^.0-9]", "", number)))
+
+                number = bs.find(
+                    "div", {"data-testid": "table-value-rooms_num"}
+                ).text.replace(",", ".")
+                listing["rooms"] = int(float(re.sub(r"[^.0-9]", "", number)))
+
+                number = bs.find(
+                    "div", {"data-testid": "table-value-area"}
+                ).text.replace(",", ".")
+                listing["area"] = int(float(re.sub(r"[^.0-9]", "", number)))
+
+                listing["estate_agency"] = bs.find(
+                    "div", {"data-testid": "table-value-advertiser_type"}
+                ).text
+
+                return listing
+
+            except Exception:
+                print("Listing URL:{url} is missing something", file=sys.stderr)
+
         return None
 
-    def scrap_listings(self, url, check_all_pages=False):
-        bs = self.getPage(url)
+    def scrap_listings(self, url, params={}):
+        bs, url = self.getPage(url, params=params)
+        print(f"Base URL: {url}")
 
         if bs is not None:
-            listings_url = set()
-            listing_json = []
+            processed_urls = set()
+            scraped_data = []
 
-            number_of_pages = 1
-            if check_all_pages:
-                page_numeration = bs.find_all(
-                    "a", {"class": "eo9qioj1 css-5tvc2l edo3iif1"}
-                )
-                number_of_pages = max([int(num.text) for num in page_numeration])
+            number_of_pages = int(
+                bs.find("nav", {"data-cy": "pagination"}).find_all("a")[-1].text
+            )
 
             for page_number in range(1, number_of_pages + 1):
-                print(page_number)
-                listing_links = self.getLinks(url + "?page={}".format(page_number))
-                # listing_links = self.getLinks(url)
-                # promoted ads
-                for listing_url in listing_links[0]:
-                    if listing_url not in listings_url:
-                        listings_url.add(listing_url)
-                        listing_json.append(self.getListing(listing_url, promoted=True))
+                print(f"Scraping page nr: {page_number}...")
 
-                # organic ads
+                listing_links = self.getLinks(url + "&page={}".format(page_number))
+
+                # promoted_urls ads
+                for listing_url in listing_links[0]:
+                    if listing_url not in processed_urls:
+                        processed_urls.add(listing_url)
+                        result = self.getListing(listing_url, promoted=True)
+                        if result is not None:
+                            scraped_data.append(result)
+
+                # organic_urls ads
                 for listing_url in listing_links[1]:
-                    if listing_url not in listings_url:
-                        listings_url.add(listing_url)
-                        listing_json.append(
-                            self.getListing(listing_url, promoted=False)
-                        )
+                    if listing_url not in processed_urls:
+                        processed_urls.add(listing_url)
+                        result = self.getListing(listing_url, promoted=False)
+                        if result is not None:
+                            scraped_data.append(result)
 
             with open("otodom_listing.json", "w", encoding="utf-8") as json_file:
-                json.dump(listing_json, json_file, ensure_ascii=False, indent=2)
+                json.dump(scraped_data, json_file, ensure_ascii=False, indent=2)
 
     def generate_url(self):
         with open("otodom_settings.json") as f:
             data = json.load(f)
+
             url = data["base_url"] + "pl/wyniki"
 
             if data["only_for_sale"]:
                 url += "/sprzedaz"
-
-            if data["only_for_rent"]:
+            elif data["only_for_rent"]:
                 url += "/wynajem"
                 url += "/" + data["property_type"] + "/"
+
             if len(data["province"]) > 0:
                 url += data["province"] + "/" + data["city"] + "?"
             else:
                 url += "cala-polska?"
 
-            url += "limit=36"
+            params = {
+                "priceMin": data["price_min"],
+                "priceMax": data["price_max"],
+                "viewType": "listing",
+                "by": "LATEST",
+                "direction": "DESC",
+                "limit": "72",
+            }
 
-            if len(data["price_min"]) > 0:
-                url += "&priceMin=" + data["price_min"]
-
-            if len(data["price_max"]) > 0:
-                url += "&priceMax=" + data["price_max"]
-
-            url += "&by=LATEST&direction=DESC&viewType=listing"
-            # print("Generated link:\n", url)
-            return url
+            return (url, params)
 
 
 if __name__ == "__main__":
     crawler = Crawler()
 
-    if len(sys.argv) > 2 and sys.argv[1] == "-u":
-        print(sys.argv[2])
-        crawler.scrap_listings(sys.argv[2], check_all_pages=False)
+    parser = argparse.ArgumentParser(description="Otodom Listing Scrapper")
+    parser.add_argument(
+        "-u", "--url", help="Specify the base URL for scraping listings"
+    )
+    parser.add_argument(
+        "-s",
+        "--settings",
+        nargs="?",
+        const=True,
+        help="Generate base URL from otodom_setting.json",
+    )
+    args = parser.parse_args()
+
+    if args.url:
+        crawler.scrap_listings(args.url)
+    elif args.settings:
+        print("URL generated from otodom_settings.json")
+        crawler.scrap_listings(*crawler.generate_url())
     else:
-        crawler.scrap_listings(crawler.generate_url(), check_all_pages=False)
+        url = input("Enter URL: ")
+        crawler.scrap_listings(url)
